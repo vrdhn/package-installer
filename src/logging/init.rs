a89fe19f962317623cf2c2a88f9c00e5d04227f3
Vardhan <vrdhn0@gmail.com>
2026-03-01 22:30:32 +0530

    feat: enable millisecond timestamps in logs

--- src/logging/init.rs
+++ src/logging/init.rs
@@ -13,7 +13,7 @@ pub fn init_logging(quiet: bool, verbose: bool, debug: bool) {
 
     env_logger::Builder::new()
         .filter_level(log_level)
-        .format_timestamp(None)
+        .format_timestamp_millis()
         .format_target(false)
         .init();
 }
